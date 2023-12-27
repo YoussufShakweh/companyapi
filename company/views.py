e9d637b088b08f16c743dfa6cd1a08924b13e060
Youssuf Shakweh <yousef.shakwa@gmail.com>
2023-12-27 20:30:31 +0300

    Modified views.py

--- company/views.py
+++ company/views.py
@@ -1,4 +1,6 @@
 from django.db.models.aggregates import Count
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from .models import Employee, Department, Dependent
 from .serializers import (
@@ -13,6 +15,16 @@ class DepartmentViewSet(ModelViewSet):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
 
+    def destroy(self, request, *args, **kwargs):
+        if Employee.objects.filter(department_id=kwargs["pk"]).count() > 0:
+            return Response(
+                {
+                    "error": "It is not possible to delete a department where there are employees working within it."
+                },
+                status=status.HTTP_405_METHOD_NOT_ALLOWED,
+            )
+        return super().destroy(request, *args, **kwargs)
+
 
 class EmployeeViewSet(ModelViewSet):
     queryset = Employee.objects.annotate(dependents_count=Count("dependents")).all()
@@ -20,10 +32,10 @@ class EmployeeViewSet(ModelViewSet):
 
 
 class DependentViewSet(ModelViewSet):
-    http_method_names = ["get", "post", "patch", "delete"]
+    http_method_names = ["get", "post", "PUT", "delete"]
 
     def get_serializer_class(self):
-        if self.request.method == "PATCH":
+        if self.request.method == "PUT":
             return UpdateDependentSerializer
         else:
             return DependentSerializer
