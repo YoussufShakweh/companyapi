87aaa5389cb3af8c37c490b8ddc97af5d8bb2151
Youssuf Shakweh <110261583+YoussufShakweh@users.noreply.github.com>
2024-01-06 12:15:22 +0300

    Modified urls.py - Add the API docs

--- company/urls.py
+++ company/urls.py
@@ -1,5 +1,19 @@
+from django.urls import path
+from rest_framework import permissions
 from rest_framework_nested import routers
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from . import views
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Company Database API Documentation",
+        default_version="v1",
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 # ----------------------------------------------------------------------------- #
 # Routers
@@ -15,4 +29,6 @@ employees_router.register(
     "dependents", views.DependentViewSet, basename="employee-dependents"
 )
 # ----------------------------------------------------------------------------- #
-urlpatterns = router.urls + employees_router.urls
+urlpatterns = router.urls + employees_router.urls + [
+    path("docs/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-redoc"),
+]
