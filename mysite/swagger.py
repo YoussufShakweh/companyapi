f8051d1e026fc34d0617d5382154d87d130a12fb
Youssuf Shakweh <yousef.shakwa@gmail.com>
2024-01-07 12:33:34 +0300

    Solve Nested Router Problem

--- mysite/swagger.py
+++ mysite/swagger.py
@@ -3,4 +3,4 @@ from drf_yasg.inspectors import SwaggerAutoSchema
 
 class CompoundTagsSchema(SwaggerAutoSchema):
     def get_tags(self, operation_keys):
-        return [" > ".join(operation_keys[:-1])]
+        return ["_".join(operation_keys[:-1])]
