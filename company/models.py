065c05955073e223665f4cd0e4bfe246655f225e
Youssuf Shakweh <yousef.shakwa@gmail.com>
2024-01-08 19:06:46 +0300

    Manager Relationship - Department

--- company/models.py
+++ company/models.py
@@ -3,6 +3,14 @@ from django.db import models
 
 class Department(models.Model):
     name = models.CharField(max_length=150, unique=True)
+    manager = models.OneToOneField(
+        "Employee",
+        on_delete=models.CASCADE,
+        related_name="managers",
+        blank=True,
+        null=True,
+    )
+    management_start_date = models.DateField(null=True)
 
     def __str__(self) -> str:
         return self.name
