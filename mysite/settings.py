907eff2b5a46e06b6c3b84b71559ef5ea026bd12
Youssuf Shakweh <110261583+YoussufShakweh@users.noreply.github.com>
2024-01-09 12:17:58 +0300

    Add django_filters app

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_filters",
     "debug_toolbar",
     "rest_framework",
     "drf_yasg",
