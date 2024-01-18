89f24ba85551caa2428d9e5352c09c45575c286e
Youssuf Shakweh <110261583+YoussufShakweh@users.noreply.github.com>
2024-01-18 17:50:45 +0300

    Django Version

--- mysite/settings.py
+++ mysite/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-a$v2(sa86nqw@t-o_#o@ym6i87&s%@+qu2k$1ghq5nphx1*9^a"
